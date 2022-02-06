972275adb146f57e877b430f116ca062eddaa36e
su-san1129 <deviationvalue1023@outlook.jp>
2022-02-06 18:28:46 +0900

    Prettier を適用

--- suitter-client/src/app/store.ts
+++ suitter-client/src/app/store.ts
@@ -1,4 +1,4 @@
-import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {},
@@ -6,7 +6,9 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
-    RootState,
-    unknown,
-    Action<string>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
