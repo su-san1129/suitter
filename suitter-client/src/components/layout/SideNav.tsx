2dbe951c3909b89ed3ae6ce2d9ae91ed90a87cba
su-san1129 <deviationvalue1023@outlook.jp>
2022-02-08 00:19:51 +0900

    メインメニューにボタンを追加

--- suitter-client/src/components/layout/SideNav.tsx
+++ suitter-client/src/components/layout/SideNav.tsx
@@ -2,6 +2,9 @@ import { UserIcon } from '../../features/users/components/UserIcon';
 import homeIcon from 'assets/home.svg';
 import profileIcon from 'assets/user-regular.svg';
 import { css } from '@emotion/react';
+import { colorStyles } from '../../styles/styles';
+import { Button } from '../elements/button/Button';
+import styled from '@emotion/styled';
 
 export const SideNav = () => {
   const navList = [
@@ -11,7 +14,7 @@ export const SideNav = () => {
 
   return (
     <header className="SideNav" css={sideNavContainerStyle}>
-      <h1>Suitter</h1>
+      <h1 css={{ marginBottom: '32px' }}>Suitter</h1>
       <nav>
         {navList.map((nav) => (
           <li css={listStyle} key={nav.label}>
@@ -22,6 +25,24 @@ export const SideNav = () => {
           </li>
         ))}
       </nav>
+      <Button
+        type="submit"
+        css={{
+          margin: '8px 0',
+          width: '80%',
+          height: '40px',
+          backgroundColor: '#1d9bf0',
+          borderRadius: '16px',
+          color: '#FFF',
+          fontSize: '18px',
+          fontWeight: '700',
+          ':hover': {
+            opacity: '0.8',
+          },
+        }}
+      >
+        投稿する
+      </Button>
       <UserIcon></UserIcon>
     </header>
   );
@@ -29,16 +50,21 @@ export const SideNav = () => {
 
 const sideNavContainerStyle = css({
   width: '275px',
+  padding: '16px',
 });
 
 const listStyle = css({
   display: 'flex',
   alignItems: 'center',
-  padding: '8px 0',
+  padding: '16px',
+  ':hover': {
+    backgroundColor: colorStyles.HOVER_COLOR_PRIMARY,
+    cursor: 'pointer',
+  },
 });
 
 const navIcon = css({
-  width: '25px',
-  height: '25px',
+  width: '24px',
+  height: '24px',
   marginRight: '16px',
 });
