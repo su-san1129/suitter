5c12c2cb5230d6b7bf612f9d327657581fc84792
su-san1129 <deviationvalue1023@outlook.jp>
2022-02-11 17:56:54 +0900

    ユーザーの一覧を取得するエンドポイントの作成

--- suitter-api/app/controllers/UserController.scala
+++ suitter-api/app/controllers/UserController.scala
@@ -9,8 +9,14 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class UserController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+  val userList = (1 to 5).map(i => User("user-id" + i, "user-" + i, "user-" + i + "@example.com", "password", 123, isPrivate = true, "no-icon"))
+
+  def getUsers: Action[AnyContent] = Action { request =>
+    Ok(Json.toJson(userList))
+  }
+
   def getUserById(id: String): Action[AnyContent] = Action { request =>
-    val user = User("name", "email", "password", 123, isPrivate = true, "no-icon")
+    val user = userList.find(u => u.id.equals(id))
     Ok(Json.toJson(user))
   }
 
