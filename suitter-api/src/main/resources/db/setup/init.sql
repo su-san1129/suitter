7751e0a1a9c7649b68481cffb0ef8e9c477303fa
su-san1129 <deviationvalue1023@outlook.jp>
2022-03-03 22:47:47 +0900

    SQLに制約を追加

--- suitter-api/src/main/resources/db/setup/init.sql
+++ suitter-api/src/main/resources/db/setup/init.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS users
 (
     id           VARCHAR(36) NOT NULL PRIMARY KEY,
     name         VARCHAR,
-    email        VARCHAR,
-    password     VARCHAR,
+    email        VARCHAR NOT NULL,
+    password     VARCHAR NOT NULL,
     phone_number VARCHAR,
-    is_private   BOOLEAN,
+    is_private   BOOLEAN DEFAULT FALSE,
     icon         VARCHAR,
     created_at   LONG,
     updated_at   LONG
